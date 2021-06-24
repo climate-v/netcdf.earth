8ee16e68065e63343ac4cdcec8f5256aa48bd966
Tim Hagemann <tim@eternalwings.de>
2021-06-24 17:23:28 +0200

    Emit an ES6 module from webassembly

--- web/api.js
+++ web/api.js
@@ -1,3 +1,5 @@
+import Module from './visualize';
+
 const VALUE_TYPES = [
     'i8',
     'i16',
@@ -6,173 +8,195 @@ const VALUE_TYPES = [
     'u16',
     'u32',
     'f32',
-    'f64',
+    'f64'
 ];
 
-const ALLOCATION_CONFIG = {
-    'i32': {
-        heap: Module.HEAP32,
-        array: Int32Array
-    },
-    'i16': {
-        heap: Module.HEAP16,
-        array: Int16Array
-    },
-    'i8': {
-        heap: Module.HEAP8,
-        array: Int8Array
-    },
-    'u32': {
-        heap: Module.HEAPU32,
-        array: Uint32Array
-    },
-    'u16': {
-        heap: Module.HEAPU16,
-        array: Uint16Array
-    },
-    'u8': {
-        heap: Module.HEAPU8,
-        array: Uint8Array
-    },
-    'f32': {
-        heap: Module.HEAPF32,
-        array: Float32Array
-    },
-    'f64': {
-        heap: Module.HEAPF64,
-        array: Float64Array
+function setupAllocationConfig(moduleInstance) {
+    return {
+        'i32': {
+            heap: moduleInstance.HEAP32,
+            array: Int32Array
+        },
+        'i16': {
+            heap: moduleInstance.HEAP16,
+            array: Int16Array
+        },
+        'i8': {
+            heap: moduleInstance.HEAP8,
+            array: Int8Array
+        },
+        'u32': {
+            heap: moduleInstance.HEAPU32,
+            array: Uint32Array
+        },
+        'u16': {
+            heap: moduleInstance.HEAPU16,
+            array: Uint16Array
+        },
+        'u8': {
+            heap: moduleInstance.HEAPU8,
+            array: Uint8Array
+        },
+        'f32': {
+            heap: moduleInstance.HEAPF32,
+            array: Float32Array
+        },
+        'f64': {
+            heap: moduleInstance.HEAPF64,
+            array: Float64Array
+        }
     }
 }
 
-function allocArray(type, array) {
-    let conf = ALLOCATION_CONFIG[type];
-    if (conf == null) {
-        throw new Error("Could not find allocation configuration for type: " + type);
-    }
+function setupAllocArray(module, allocationConfiguration) {
+    return (type, array) => {
+        let conf = allocationConfiguration[type];
+        if(conf == null) {
+            throw new Error("Could not find allocation configuration for type: " + type);
+        }
 
-    const arrayCreator = conf.array;
-    const size = array.length;
-    const byteSize = arrayCreator.BYTES_PER_ELEMENT;
-    const offset = Module._malloc(size * byteSize);
-    const alignedOffset = offset / byteSize;
-    conf.heap.set(new arrayCreator(array), alignedOffset);
-    return {
-        data: conf.heap.subarray(alignedOffset, alignedOffset + size),
-        offset
+        const arrayCreator = conf.array;
+        const size = array.length;
+        const byteSize = arrayCreator.BYTES_PER_ELEMENT;
+        const offset = module._malloc(size * byteSize);
+        const alignedOffset = offset / byteSize;
+        conf.heap.set(new arrayCreator(array), alignedOffset);
+        return {
+            data: conf.heap.subarray(alignedOffset, alignedOffset + size),
+            offset
+        };
     };
 }
 
-function cwrapArrayParams(callName, returnType, argTypes, arrayTypes) {
-    let arrayIndices = [];
-    let targetArgTypes = [];
-    for (let i = argTypes.length - 1; i >= 0; i--) {
-        const currentType = argTypes[i];
-        if (currentType === 'array') {
-            arrayIndices = arrayIndices.map(index => index + 1);
-            arrayIndices.unshift(i);
-            targetArgTypes.unshift('number', 'number');
-        } else {
-            targetArgTypes.unshift(currentType);
-        }
-    }
-
-    const typeOfArrayNr = (number) => {
-        if (arrayTypes == null || arrayTypes.length === 0 || arrayTypes.length <= number) {
-            return 'i32';
-        } else {
-            return arrayTypes[number];
-        }
-    }
-
-    const wrap = Module.cwrap(callName, returnType, targetArgTypes);
-
-    return (...args) => {
-        const newArgs = [...args];
-        let arrayCount = 0;
-        const buffers = [];
-        for (let index of arrayIndices) {
-            const arr = newArgs[index];
-            const type = typeOfArrayNr(arrayCount);
-            const allocation = allocArray(type, arr);
-            newArgs[index] = allocation.offset;
-            buffers.push(allocation.offset);
-            newArgs.splice(index + 1, 0, arr.length);
-            arrayCount++;
-        }
-
-        let result;
-        try {
-            result = wrap(...newArgs);
-        } finally {
-            for (let buffer of buffers) { // is this necessary? rust should free it on deref
-                Module._free(buffer);
+function setupCwrapArrayParams(module, allocArray) {
+    return (callName, returnType, argTypes, arrayTypes) => {
+        let arrayIndices = [];
+        let targetArgTypes = [];
+        for(let i = argTypes.length - 1; i >= 0; i--) {
+            const currentType = argTypes[i];
+            if(currentType === 'array') {
+                arrayIndices = arrayIndices.map(index => index + 1);
+                arrayIndices.unshift(i);
+                targetArgTypes.unshift('number', 'number');
+            } else {
+                targetArgTypes.unshift(currentType);
             }
         }
 
-        return result;
-    }
-}
+        const typeOfArrayNr = (number) => {
+            if(arrayTypes == null || arrayTypes.length === 0 || arrayTypes.length <= number) {
+                return 'i32';
+            } else {
+                return arrayTypes[number];
+            }
+        }
 
-function cwrapArrayReturn(cwrap, type) {
-    const conf = ALLOCATION_CONFIG[type];
-    if(conf == null) {
-        throw new Error("Could not find allocation configuration for type: " + type);
-    }
+        const wrap = module.cwrap(callName, returnType, targetArgTypes);
 
-    const usedHeap = conf.heap;
-    const arrayCreator = conf.array;
-    return (...args) => {
-        const result = cwrap(...args);
-        const arrayPointer = Module.HEAPU32[result / Uint32Array.BYTES_PER_ELEMENT];
-        const arrayLen = Module.HEAPU32[result / Uint32Array.BYTES_PER_ELEMENT + 1];
-        const array = new arrayCreator(usedHeap.buffer, arrayPointer, arrayLen);
-        const newReturn = [...array];
-        api.dropBytes(result);
-        return newReturn;
+        return (...args) => {
+            const newArgs = [...args];
+            let arrayCount = 0;
+            const buffers = [];
+            for(let index of arrayIndices) {
+                const arr = newArgs[index];
+                const type = typeOfArrayNr(arrayCount);
+                const allocation = allocArray(type, arr);
+                newArgs[index] = allocation.offset;
+                buffers.push(allocation.offset);
+                newArgs.splice(index + 1, 0, arr.length);
+                arrayCount++;
+            }
+
+            let result;
+            try {
+                result = wrap(...newArgs);
+            } finally {
+                for(let buffer of buffers) { // is this necessary? rust should free it on deref
+                    module._free(buffer);
+                }
+            }
+
+            return result;
+        }
     };
 }
 
-const api = {
-    openFile: Module.cwrap('open_file', 'boolean', ['string']),
-    getStringAttribute: Module.cwrap('get_string_attribute', 'string', ['string']),
-    getVariables: Module.cwrap('get_variables', 'string', []),
-    getDimensions: Module.cwrap('get_dimensions', 'string', []),
-    closeFile: Module.cwrap('close_file', null, []),
-    getVariableDimensions: Module.cwrap('get_variable_dimensions', 'string', ['string']),
-    dropBytes: Module.cwrap('drop_bytes', null, ['number']),
-    getVariableType: Module.cwrap('get_variable_type', 'string', ['string']),
-    getVariableStringAttribute: Module.cwrap('get_variable_string_attribute', 'string', ['string', 'string']),
-    getDimensionLength: Module.cwrap('get_dimension_len', 'number', ['string'])
+function setupCwrapArrayReturn(module, allocationConfig) {
+    return (api, cwrap, type) => {
+        const conf = allocationConfig[type];
+        if(conf == null) {
+            throw new Error("Could not find allocation configuration for type: " + type);
+        }
+
+        const usedHeap = conf.heap;
+        const arrayCreator = conf.array;
+        return (...args) => {
+            const result = cwrap(...args);
+            const arrayPointer = module.HEAPU32[result / Uint32Array.BYTES_PER_ELEMENT];
+            const arrayLen = module.HEAPU32[result / Uint32Array.BYTES_PER_ELEMENT + 1];
+            const array = new arrayCreator(usedHeap.buffer, arrayPointer, arrayLen);
+            const newReturn = [...array];
+            api.dropBytes(result);
+            return newReturn;
+        };
+    };
 }
 
-api.getTitle = () => api.getStringAttribute('title');
-api.getVariableValue = (variable, ...args) => {
-    const variableType = api.getVariableType(variable);
-    return api[`getVariableValue${variableType.toUpperCase()}`](variable, ...args);
-};
-api.getVariableValues = (variable, ...args) => {
-    const variableType = api.getVariableType(variable);
-    return api[`getVariableValues${variableType.toUpperCase()}`](variable, ...args);
-};
+function constructApi(moduleInstance) {
+    const ALLOCATION_CONFIG = setupAllocationConfig(moduleInstance);
+    const allocArray = setupAllocArray(moduleInstance, ALLOCATION_CONFIG);
+    const cwrapArrayParams = setupCwrapArrayParams(moduleInstance, allocArray);
+    const cwrapArrayReturn = setupCwrapArrayReturn(moduleInstance, ALLOCATION_CONFIG);
 
-api.getAllVariableValues = (variable) => {
-    const variableType = api.getVariableType(variable);
-    return api[`getAllVariableValues${variableType.toUpperCase()}`](variable);
-};
+    const api = {
+        FS: moduleInstance.FS,
+        createNetcdfFile(data) {
+            return new NetCDFFile(this, data);
+        },
+        openFile: moduleInstance.cwrap('open_file', 'boolean', ['string']),
+        getStringAttribute: moduleInstance.cwrap('get_string_attribute', 'string', ['string']),
+        getVariables: moduleInstance.cwrap('get_variables', 'string', []),
+        getDimensions: moduleInstance.cwrap('get_dimensions', 'string', []),
+        closeFile: moduleInstance.cwrap('close_file', null, []),
+        getVariableDimensions: moduleInstance.cwrap('get_variable_dimensions', 'string', ['string']),
+        dropBytes: moduleInstance.cwrap('drop_bytes', null, ['number']),
+        getVariableType: moduleInstance.cwrap('get_variable_type', 'string', ['string']),
+        getVariableStringAttribute: moduleInstance.cwrap('get_variable_string_attribute', 'string', ['string', 'string']),
+        getDimensionLength: moduleInstance.cwrap('get_dimension_len', 'number', ['string'])
+    }
 
-for (let varType of VALUE_TYPES) {
-    api[`getVariableValues${varType.toUpperCase()}`] = cwrapArrayReturn(cwrapArrayParams(`get_${varType}_values_for`, 'number', ['string', 'array', 'array'], ['i32', 'i32']), varType);
-    api[`getAllVariableValues${varType.toUpperCase()}`] = (variable) => api[`getVariableValues${varType.toUpperCase()}`](variable, [], 0, [], 0);
-    api[`getVariableValue${varType.toUpperCase()}`] = cwrapArrayParams(`get_${varType}_value_for`, 'number', ['string', 'array'], ['i32']);
+    api.getTitle = () => api.getStringAttribute('title');
+    api.getVariableValue = (variable, ...args) => {
+        const variableType = api.getVariableType(variable);
+        return api[`getVariableValue${variableType.toUpperCase()}`](variable, ...args);
+    };
+    api.getVariableValues = (variable, ...args) => {
+        const variableType = api.getVariableType(variable);
+        return api[`getVariableValues${variableType.toUpperCase()}`](variable, ...args);
+    };
+
+    api.getAllVariableValues = (variable) => {
+        const variableType = api.getVariableType(variable);
+        return api[`getAllVariableValues${variableType.toUpperCase()}`](variable);
+    };
+
+    for(let varType of VALUE_TYPES) {
+        api[`getVariableValues${varType.toUpperCase()}`] = cwrapArrayReturn(api, cwrapArrayParams(`get_${varType}_values_for`, 'number', ['string', 'array', 'array'], ['i32', 'i32']), varType);
+        api[`getAllVariableValues${varType.toUpperCase()}`] = (variable) => api[`getVariableValues${varType.toUpperCase()}`](variable, [], 0, [], 0);
+        api[`getVariableValue${varType.toUpperCase()}`] = cwrapArrayParams(`get_${varType}_value_for`, 'number', ['string', 'array'], ['i32']);
+    }
+    return api;
 }
 
 class NetCDFFile {
-    constructor({ type, ref, name = "" }) {
+    constructor(api, { type, ref, name = "" }) {
+        this.api = api;
+        this.FS = api.FS;
         this.fileRef = ref;
         this.fileType = type;
         this.mountedPath = null;
         this.isOpen = false;
-        if (type === 'file') {
+        if(type === 'file') {
             this.name = ref.name;
         } else {
             this.name = name;
@@ -182,15 +206,15 @@ class NetCDFFile {
     async _mount() {
         const path = `/${this.name}`;
         let buffer;
-        if (this.fileType === 'buffer') {
+        if(this.fileType === 'buffer') {
             buffer = this.fileRef;
         } else {
             buffer = await this.fileRef.arrayBuffer();
         }
-        const fp = FS.open(path, 'w+');
+        const fp = this.FS.open(path, 'w+');
         const view = new Uint8Array(buffer);
-        FS.write(fp, view, 0, view.length, 0);
-        FS.close(fp);
+        this.FS.write(fp, view, 0, view.length, 0);
+        this.FS.close(fp);
         this.mountedPath = path;
     }
 
@@ -199,34 +223,36 @@ class NetCDFFile {
     }
 
     async open() {
-        if (!this._isMounted()) {
+        if(!this._isMounted()) {
             await this._mount();
         }
 
-        this.isOpen = api.openFile(this.mountedPath);
+        this.isOpen = this.api.openFile(this.mountedPath);
         return this.isOpen;
     }
 
     _unmount() {
-        FS.unlink(this.mountedPath);
+        this.FS.unlink(this.mountedPath);
         this.mountedPath = null;
     }
 
     close() {
-        if (this.isOpen) {
-            api.closeFile();
+        if(this.isOpen) {
+            this.api.closeFile();
         }
 
-        if (this.mountedPath != null) {
+        if(this.mountedPath != null) {
             this._unmount();
         }
     }
 }
 
-function waitModuleInit() {
-    return new Promise((resolve) => {
-        Module.onRuntimeInitialized = () => {
-            resolve();
-        };
+export function createApi() {
+    return Module({
+        locateFile(path) {
+            return "/" + path;
+        }
+    }).then(module => {
+        return constructApi(module);
     });
 }
